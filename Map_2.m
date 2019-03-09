495fa26d5dbd232ef72a5aa56877cb125a983b99
Luke <luke.markham@tessella.com>
2019-03-09 13:07:59 +0000

    Work in progress

--- Map_2.m
+++ Map_2.m
@@ -2,14 +2,17 @@ clear all
 clear
 
 [~,~,pop_data] = xlsread('population_density_by_area_2.xlsx');
-LSOA_Boundaries = shaperead('LLSOA_2001_Boundaries.shp','UseGeoCoords', true);
+pop_data_table = cell2table(pop_data(2:end,:));
+pop_data_table.Properties.VariableNames = ["area_code", "all_ages"];
 
+% LSOA_Boundaries = shaperead('LLSOA_2001_Boundaries.shp','UseGeoCoords', true);
+LSOA_Boundaries_table = struct2table(LSOA_Boundaries);
 
-for n=1:34378
-    fill(LSOA_Boundaries(n).Lon,LSOA_Boundaries(n).Lat,'r');
-    hold on
-end
-hold off
+%for n=1:34378
+%    fill(LSOA_Boundaries(n).Lon,LSOA_Boundaries(n).Lat,'r');
+%    hold on
+%end
+%hold off
 
 % LSOA_Boundaries = shaperead('LAD_Pop_EngWales2011.shp', 'UseGeoCoords', true);
 % figure
